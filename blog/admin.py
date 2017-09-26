4520501370d8bc7157ab0d1181ee781ed309e8ad
cgDeepLearn <xiujiaw@gmail.com>
2017-09-26 13:47:20 +0800

    fix model

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,9 @@ class ArticleAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_time'
     list_display = ('title', 'category', 'author', 'date_time', 'view')
     list_filter = ('category', 'author')
+    search_fields = ('title', 'content')
     filter_horizontal = ('tag',)
+    odering = ['date_time']
 
 
 @admin.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@
 -------------------------------------------------
    File Name：     models.py
    Description :
-   Author :       JHao
+   Author :       cgDeepLearn
    date：          2016/11/18
 -------------------------------------------------
    Change Activity:
@@ -13,6 +13,7 @@
 
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 
 # Create your models here.
@@ -20,23 +21,28 @@ from django.conf import settings
 class Tag(models.Model):
     tag_name = models.CharField(u'标签名', max_length=30)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.tag_name
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200)  # 博客标题
-    category = models.ForeignKey('Category', verbose_name=u'文章类型', on_delete=models.CASCADE)
-    date_time = models.DateField(auto_now_add=True)  # 博客日期
-    content = models.TextField(blank=True, null=True)  # 文章正文
-    digest = models.TextField(blank=True, null=True)  # 文章摘要
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=u'作者', on_delete=models.CASCADE)
-    view = models.BigIntegerField(default=0)  # 阅读数
-    comment = models.BigIntegerField(default=0)  # 评论数
-    picture = models.CharField(max_length=200)  # 标题图片地址
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作者', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', verbose_name='文章分类', on_delete=models.CASCADE)
+    date_time = models.DateField(default=timezone.now, verbose_name='创建时间')  # 博客日期
+    content = models.TextField(blank=True, null=True, verbose_name='正文')  # 文章正文
+    digest = models.TextField(blank=True, null=True, verbose_name='摘要')  # 文章摘要
+    picture = models.CharField(max_length=200, verbose_name='题图')  # 标题图片地址
+    view = models.BigIntegerField(default=0, verbose_name='阅读数量')  # 阅读数
+    comment = models.BigIntegerField(default=0, verbose_name='评论数')  # 评论数
+    
     tag = models.ManyToManyField(Tag)  # 标签
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     def sourceUrl(self):
@@ -61,19 +67,21 @@ class Article(models.Model):
 
     class Meta:  # 按时间降序
         ordering = ['-date_time']
+        verbose_name = u"文章"
+        verbose_name_plural = verbose_name
 
 
 class Category(models.Model):
-    name = models.CharField(u'文章类型', max_length=30)
+    name = models.CharField(u'分类名称', max_length=30)
     created_time = models.DateTimeField(u'创建时间', auto_now_add=True)
     last_mod_time = models.DateTimeField(u'修改时间', auto_now=True)
 
     class Meta:
         ordering = ['name']
-        verbose_name = u"文章类型"
+        verbose_name = u"分类"
         verbose_name_plural = verbose_name
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
