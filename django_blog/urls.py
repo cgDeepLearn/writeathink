b1b9a1d75e608da48fb6d4ded9892cc3c0b401b2
cgDeepLearn <xiujiaw@gmail.com>
2017-09-26 15:02:49 +0800

    fix static url

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from blog import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,4 +26,4 @@ urlpatterns = [
     url(r'^blog/$', views.Index, name='index'),
     url(r'^$', views.Index),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
